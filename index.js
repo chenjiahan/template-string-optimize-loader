8a605698b91e46d1760b0ab367c8577b8179ddbc
陈嘉涵 <chenjiahan@meituan.com>
2017-01-19 19:25:23 +0800

    release 2.0.0

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const acorn = require('acorn');
+const escodegen = require('escodegen');
+
 module.exports = function(source) {
-    return source.replace(/(\\n\s*)/g, '');
+    const tree = acorn.parse(source, { sourceType: 'module' });
+    traverse(tree);
+    return escodegen.generate(tree);
 };
+
+function isObject(item) {
+    return Object.prototype.toString.call(item) === '[object Object]';
+}
+
+function traverse(input) {
+    if (Array.isArray(input)) {
+        input.forEach(item => traverse(item));
+    } 
+    else if (isObject(input)) {
+        for (let key in input) {
+            if (typeof input[key] === 'string') {
+                input[key] = input[key].replace(/(\n\s*)/g, '');
+            }
+            traverse(input[key]);
+        }
+    }
+}
